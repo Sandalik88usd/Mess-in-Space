91feefdaa7383fa71808d28ca58a065912007fdc
CYKERO4KA <cykero4ka88@gmail.com>
2023-02-07 17:33:31 +0200

    Add rotation

--- Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
+++ Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Quaternion = System.Numerics.Quaternion;
-using UnityEngine.Quaternion;
 
 public class PlayerMoveMent : MonoBehaviour
 {
@@ -18,14 +16,13 @@ public class PlayerMoveMent : MonoBehaviour
     {
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.z = Input.GetAxisRaw("Vertical");
-        _cursorInput.x = Input.GetAxisRaw("Mouse X");
-        _cursorInput.y = Input.GetAxisRaw("Mouse Y");
+        _cursorInput.x += Input.GetAxisRaw("Mouse X");
+        _cursorInput.y += Input.GetAxisRaw("Mouse Y");
     }
 
     private void FixedUpdate()
     {
         _rigidbody.MovePosition(_rigidbody.position + _movement.normalized * Time.fixedDeltaTime * speed);
-        Debug.Log(_cursorInput);
-        transform.rotation = new Quaternion( _cursorInput * Time.fixedDeltaTime);
+        transform.localRotation = Quaternion.Euler(0, _cursorInput.x, 0);
     }
 }
