831454c1523bf716160e644570e5fdd90a430a81
CYKERO4KA <cykero4ka88@gmail.com>
2023-02-07 17:13:45 +0200

    Edit player movement script and add ground platform.

--- Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
+++ Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
@@ -2,22 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Quaternion = System.Numerics.Quaternion;
+using UnityEngine.Quaternion;
 
 public class PlayerMoveMent : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5.0f;
-    private Rigidbody _rigidbody;
+    [SerializeField] private float speed = 5.0f;
+    public Rigidbody _rigidbody;
     private Vector3 _movement;
+    private Vector2 _cursorInput;
+    
 
     // Update is called once per frame
     void Update()
     {
-        _movement.z = Input.GetAxis("Horizontal");
-        _movement.x = Input.GetAxis("Vertical");
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.z = Input.GetAxisRaw("Vertical");
+        _cursorInput.x = Input.GetAxisRaw("Mouse X");
+        _cursorInput.y = Input.GetAxisRaw("Mouse Y");
     }
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = _movement * Time.deltaTime * _speed;
+        _rigidbody.MovePosition(_rigidbody.position + _movement.normalized * Time.fixedDeltaTime * speed);
+        Debug.Log(_cursorInput);
+        transform.rotation = new Quaternion( _cursorInput * Time.fixedDeltaTime);
     }
 }
