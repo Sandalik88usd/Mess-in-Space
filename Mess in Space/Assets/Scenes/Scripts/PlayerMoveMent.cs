3af8fd9532b1a40a914ec5eb5a9d9dee8c6a5fa9
CYKERO4KA <cykero4ka88@gmail.com>
2023-02-07 11:40:54 +0200

    Edit movement script

--- Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
+++ Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoveMent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _speed = 5.0f;
+    private Rigidbody _rigidbody;
+    private Vector3 _movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        _movement.z = Input.GetAxis("Horizontal");
+        _movement.x = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _movement * Time.deltaTime * _speed;
     }
 }
