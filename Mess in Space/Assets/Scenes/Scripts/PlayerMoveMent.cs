e1c7aa255afe8f817fe907bae69c1f71cbfee03f
CYKERO4KA <cykero4ka88@gmail.com>
2023-02-07 18:36:38 +0200

    Add jump system

--- Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
+++ Mess in Space/Assets/Scenes/Scripts/PlayerMoveMent.cs
@@ -6,23 +6,33 @@ using UnityEngine;
 public class PlayerMoveMent : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpForce = 3.0f;
     public Rigidbody _rigidbody;
     private Vector3 _movement;
     private Vector2 _cursorInput;
-    
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.z = Input.GetAxisRaw("Vertical");
+        _movement.y = Input.GetAxisRaw("Jump");
         _cursorInput.x += Input.GetAxisRaw("Mouse X");
         _cursorInput.y += Input.GetAxisRaw("Mouse Y");
+        
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(transform.position + _movement.normalized * Time.fixedDeltaTime * speed);
+        transform.Translate(_movement.normalized * Time.fixedDeltaTime * speed);
+        _rigidbody.AddForce(_rigidbody.velocity.x,_movement.y * Time.fixedDeltaTime * jumpForce, _rigidbody.velocity.z);
+        //_rigidbody.MovePosition(_rigidbody.position +_movement.normalized * Time.fixedDeltaTime * speed);
         transform.localRotation = Quaternion.Euler(0, _cursorInput.x, 0);
+        //_rigidbody.velocity = new Vector3(_xMovement, _rigidbody.velocity.y, _zMovement).normalized  * Time.deltaTime * speed;
     }
 }
